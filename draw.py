3e9c1f582f387ce0478403a267249461a0ac5196
jinops <jinwoo.develop@gmail.com>
2023-11-30 20:11:14 +0900

    add 'nil' node drawing

--- draw.py
+++ draw.py
@@ -1,27 +1,32 @@
 import graphviz
 
 def draw(tree):
-    dot = graphviz.Digraph()
+    graph = graphviz.Digraph()
 
     def add_node(node):
         if tree.focused == node:
-            dot.node(str(node.key), color=node.color, fillcolor='#FEF9E7', style='filled')
+            graph.node(str(node.key), color=node.color, fillcolor='#FEF9E7', style='filled')
             tree.focused == None
         else:
-            dot.node(str(node.key), color=node.color)
+            graph.node(str(node.key), color=node.color)
 
     def add_nodes_edges(node):
         if node.left:
             add_node(node.left)
-            dot.edge(str(node.key), str(node.left.key))
+            graph.edge(str(node.key), str(node.left.key))
             add_nodes_edges(node.left)
         if node.right:
             add_node(node.right)
-            dot.edge(str(node.key), str(node.right.key))
+            graph.edge(str(node.key), str(node.right.key))
             add_nodes_edges(node.right)
+        if node.left is None and node.right is None:
+            graph.node(f'nil_{node.key}', 'nil', shape='box', width='0.2', height='0.3',\
+                     fillcolor='black', fontcolor='white',style='filled')
+            graph.edge(str(node.key), f'nil_{node.key}')
+
 
     if tree.root is not None:
         add_node(tree.root)
         add_nodes_edges(tree.root)
 
-    dot.render('data/tree', view=False, format='png')
+    graph.render('data/tree', view=False, format='png')
